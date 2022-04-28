ac3d5cc730eddf7cf760ccfdb04e129bca09527c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-28 16:51:33 -0700

    add using directives for RestSharp and NewtonSoft.Json

--- TravelClient/Program.cs
+++ TravelClient/Program.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 
 namespace TravelClient
 {
