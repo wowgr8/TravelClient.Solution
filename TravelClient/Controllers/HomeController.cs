ac1588cfce4023b6785a0d350368b60bab3a49cf
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-28 17:19:19 -0700

    removed logic for displaying errors.

--- TravelClient/Controllers/HomeController.cs
+++ TravelClient/Controllers/HomeController.cs
@@ -23,11 +23,5 @@ namespace TravelClient.Controllers
         var allDestinations = Destination.GetDestinations();
         return View(allDestinations);
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
